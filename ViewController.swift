acfe4aeb12fcfd1462a3eccfb5d407366d2fa17c
thonzyy <136280932+thonzyy@users.noreply.github.com>
2023-08-07 21:33:35 +0900

    2023.08.07
    label  ,button

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HellowWorld
+//
+//  Created by 이동원 on 2023/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func changeBackgroundColorToBlack(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+    }
+    
+    
+    
+    
+
+}
+
